2da421add9e96cc3dceeac97cfc112b2ae33db4d
PabloMessina <pamessina@uc.cl>
2016-05-09 00:58:06 -0300

    fix CORS problems

--- server.js
+++ server.js
@@ -6,33 +6,22 @@ var survey = require("./routes/survey");
 var cookieParser = require('cookie-parser');
 var session = require('cookie-session');
 var csrf = require('csurf');
+var cors = require('cors');
 
 var api_prefix = '/api'
 
 app.set('port', process.env.PORT || 5000);
+
+// enable cross origin request
+// source: https://github.com/expressjs/cors
+app.use(cors());
+
+// parse urlconded bodies
 app.use(bodyParser.urlencoded({
   extended: true
 }));
 
-// to enable CSRF protection
-app.use(session({
-    secret: '5h4r1ng15C4r1ng'
-}));
-app.use(cookieParser('secret'));
-app.use(csrf());
-app.use(function (req, res, next) {
-    res.cookie("XSRF-TOKEN",req.csrfToken());
-    return next();
-});
-
-// to allow access from pages served by other servers
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
-
+// -------------
 // survey routes
 app.get(api_prefix + '/survey',        survey.allSurveys);
 app.post(api_prefix + '/survey', 	   survey.createSurvey);
@@ -40,6 +29,7 @@ app.get(api_prefix + '/survey/:id',    survey.getSurvey);
 app.put(api_prefix + '/survey/:id',    survey.updateSurvey);
 app.delete(api_prefix + '/survey/:id', survey.deleteSurvey);
 
+// sync models and run server
 models.sequelize.sync().then(function () {
   app.listen(app.get('port'), function() {
     console.log('Node app is running on port', app.get('port'));
